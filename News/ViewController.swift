689e864941ed23621a5d6238b4e716f4fe19dcf2
Igor Lungis <igorlungis@yandex.ru>
2019-10-07 14:47:50 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Igor Lungis on 10/7/19.
+//  Copyright © 2019 Igor Lungis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
